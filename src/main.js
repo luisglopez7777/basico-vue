c4d916d8045e154c579a4933223c59572ba640d0
luisglopez7777 <luis.lopez@leftfieldlabs.com>
2021-11-25 16:43:47 -0600

    ready

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "@/assets/css/tailwind.css";
+import router from "@/router";
+import { dollarFilter, percentFilter } from "@/filters";
+import Chartick from "vue-chartkick";
+import Chart from "chart.js";
+import { VueSpinners } from "@saeris/vue-spinners";
 
+Vue.use(VueSpinners);
+Vue.use(Chartick.use(Chart));
+Vue.filter("dollar", dollarFilter);
+Vue.filter("percent", percentFilter);
 Vue.config.productionTip = false;
 
 new Vue({
+  router: router,
   render: (h) => h(App),
 }).$mount("#app");
